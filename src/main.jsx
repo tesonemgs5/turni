4f7954ff7699bf61cd124f4488e8242604d1d4e0
tesonemgs5 <tua@email.com>
2026-06-08 22:36:32 +0200

    fix: supabase auth + env + dipendenze

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Auth from './Auth.jsx'
+import Auth from './auth.jsx'
 import { supabase } from './supabase.js'
 import { useState, useEffect } from 'react'
 
