02e59a725c836796b90fae792074d5d3738bcc5e
tesonemgs5 <elettricotesone@gmail.com>
2026-08-22 22:30:10 +0200

    pulizia

--- src/10.auth.jsx
+++ src/10.auth.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { supabase } from "./supabase"
+import { supabase } from "./11.supabase"
 
 export default function Auth({ onLogin }) {
   const [email, setEmail] = useState("")
