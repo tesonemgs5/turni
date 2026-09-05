20d1007efb3841617e8017729a0f4014e316a371
tesonemgs5 <elettricotesone@gmail.com>
2026-09-05 05:55:24 +0200

    Aggiunta configurazione Android/Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.turni.app',
+  appName: 'turni',
+  webDir: 'dist'
+};
+
+export default config;
